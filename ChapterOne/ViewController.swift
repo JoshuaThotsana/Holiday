ef510ce0f4a05271237db06d06f75d4cc2795a0e
Thotsana <thosana.mabotsa@umuzi.org>
2020-08-15 06:04:11 +0200

    Done with Holidays Project

--- ChapterOne/ViewController.swift
+++ ChapterOne/ViewController.swift
@@ -9,23 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
+	
+//	let v2 = UIView(frame:v1.bounds.insetBy(dx: 10, dy: 10))
 
 	override func viewDidLoad() {
 		super.viewDidLoad()  // This is template code.
 		
-		let v1 = UIView(frame: CGRect(x: 113, y: 111, width: 132, height: 194))
-		v1.backgroundColor = UIColor(red: 1, green: 0.4, blue: 1, alpha: 1)
-		
-		let v2 = UIView(frame: v1.bounds.insetBy(dx: 10, dy: 10))
-		v2.backgroundColor = UIColor(red: 0.5, green: 1, blue: 0, alpha: 1)
-		
-		self.view.addSubview(v1)
-		v1.addSubview(v2)
-		
-		v1.transform = CGAffineTransform(rotationAngle: 45 * .pi/180)
-		
+		let iv = UIImageView(image:UIImage(systemName:"moon.circle.fill"))
+		self.view.addSubview(iv)
+		iv.center = iv.superview!.bounds.center
+		iv.frame = iv.frame.integral
 	}
 
 
 }
 
+extension CGRect {
+        var center : CGPoint {
+			return CGPoint(x: self.midX, y: self.midY) }
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # ChapterOne
+# Holiday
